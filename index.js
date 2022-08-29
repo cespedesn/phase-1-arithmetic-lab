7aca0266bea499a8cb582dff867ea9b32e4e1714
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-08-29 10:39:38 -0400

    commit tree 5914e650754bbc70a9f158c3c66fb27631e16c7c

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*100);
+
+let num3 = 20;
+let num4 = 5;
+const mod = num3 / num4;
+
+let max = Math.max(11,14,16,19,20);
